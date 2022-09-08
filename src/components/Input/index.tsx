abf2b9ccaedde597074d25e485b9d4dd2d23a70f
aemanuell <antonioemanueell@gmail.com>
2022-09-08 15:11:20 -0300

    update input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,20 +1,27 @@
-import { forwardRef, InputHTMLAttributes } from "react"
+import { forwardRef, InputHTMLAttributes } from "react";
 import { RegularText } from "../Typography";
-import { InputStyleContainer, InputStyled, InputWrapper, RightText } from "./styles"
+import {
+  InputWrapper,
+  InputStyleContainer,
+  InputStyled,
+  RightText,
+} from "./styles";
 
 type InputProps = InputHTMLAttributes<HTMLInputElement> & {
-    error?: string;
-    rightText?: string;
-}
+  error?: string;
+  rightText?: string;
+};
 
-export const Input = forwardRef<HTMLInputElement, InputProps>(({error, className, rightText, ...props}, ref) => {
-    return(
-        <InputWrapper className={className}>
-            <InputStyleContainer>
-                <InputStyled {...props} ref={ref}/>
-                {rightText && <RightText>{rightText}</RightText>}
-            </InputStyleContainer>
-            {error && <RegularText size="s">{error}</RegularText>}
-        </InputWrapper>
-    )
-})
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ error, rightText, className, ...props }, ref) => {
+    return (
+      <InputWrapper className={className}>
+        <InputStyleContainer hasError={!!error}>
+          <InputStyled ref={ref} {...props} />
+          {rightText && <RightText>{rightText}</RightText>}
+        </InputStyleContainer>
+        {error && <RegularText size="s">{error}</RegularText>}
+      </InputWrapper>
+    );
+  }
+);
